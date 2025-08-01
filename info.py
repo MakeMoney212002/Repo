35e2a1d707b1de7a5610e848dfd2f85f89f61082
MakeMoney212002 <115358435+MakeMoney212002@users.noreply.github.com>
2025-08-01 15:43:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ def is_enabled(value, default):
 SESSION = environ.get("SESSION", "Media_search")
 API_ID = int(environ.get("API_ID", "21711251"))
 API_HASH = environ.get("API_HASH", "77bfefd0fe7dfc048c1f85d14c88391c")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7632405492:AAEuAAbQNlmC9DtQLskphuRTqFhf4ephDaw")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8341674553:AAHgw_M4QSvAuielbl5s5AIQj3tP3Jm8yCU")
 PORT = environ.get("PORT", "8082")
 
 # Owners
@@ -172,7 +172,7 @@ if "DYNO" in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "https://repo-2k4e.onrender.com")
+URL = environ.get("FQDN", "https://repo-2k4e.onrender.com/")
 
 # Commands
 admin_cmds = [
